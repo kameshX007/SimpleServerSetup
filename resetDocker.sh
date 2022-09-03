234ab94c2ddfc33388781a0f393196127e564433
kameshX007 <112748572+kameshX007@users.noreply.github.com>
2022-09-03 16:48:34 +0530

    Update resetDocker.sh

--- resetDocker.sh
+++ resetDocker.sh
@@ -1,6 +1,6 @@
 echo "Stopping all docker containers...">resetDocker.log;
-container=$(docker container ls -q);
+container=$(docker container ls -q)>>resetDocker.log;
 [ -z "$container" ] && echo "No running container found">>resetDocker.log || docker stop $(docker container ls -q);
 echo "Deleting all docker data...">>resetDocker.log;
-docker system prune -a -f;
+docker system prune -a -f>>resetDocker.log;
 echo "Docker reset successfull...">>resetDocker.log;
