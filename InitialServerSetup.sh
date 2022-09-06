286aaef66aa9006aa131dcdedcb2967cc5943f0c
kameshX007 <112748572+kameshX007@users.noreply.github.com>
2022-09-06 19:09:55 +0530

    Update InitialServerSetup.sh

--- InitialServerSetup.sh
+++ InitialServerSetup.sh
@@ -5,8 +5,16 @@ apt -y update;
 apt-get -y install neofetch htop nano;
 
 #Changing date to IST
+echo "Changing date to IST...">InitialServerSetup.log;
 unlink /etc/localtime;
 ln -s /usr/share/zoneinfo/Asia/Kolkata /etc/localtime;
+
+#Installig Docker
+echo "Installig Docker...">InitialServerSetup.log;
+git clone https://github.com/kameshX007/installDocker.git;
+cd installDocker;
+sh installDocker.sh;
+cd ..
 
 #Installing ucaresystem-core
 echo "Installing ucaresystem-core...">>InitialServerSetup.log;
