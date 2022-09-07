5b4022d6315ab655cf6ec78102a614f0a820be1d
kameshX007 <kameshbook@gmail.com>
2022-09-07 16:02:49 +0530

    #

--- executeAsPerConfig.sh
+++ executeAsPerConfig.sh
@@ -1,33 +1,31 @@
 #!/bin/sh
 echo "Setting up server as per server.config...";
 
-filename="server.config"
-
 #ucaresystem-core
-configData = $(sed -n '2p' server.config);
+configData=$(sed -n '2p' server.config);
 if [ $configData = 'Y' ]
 then
     sh systemUpdate.sh;
 fi
 
 #Changing date to given timestamp
-configData = $(sed -n '4p' server.config);
-selectedTimezone = $(sed -n '6p' server.config);
+configData=$(sed -n '4p' server.config);
+selectedTimezone=$(sed -n '6p' server.config);
 if [ $configData = 'Y' ]
 then
     sh systemDateUpdate.sh $selectedTimezone;
 fi
 
 ##Installing apps
-configData = $(sed -n '8p' server.config);
-selectedApps = $(sed -n '10p' server.config);
+configData=$(sed -n '8p' server.config);
+selectedApps=$(sed -n '10p' server.config);
 if [ $configData = 'Y' ]
 then
     sh systemUpdate.sh $selectedApps;
 fi
 
 #Installig Docker
-configData = $(sed -n '12p' server.config);
+configData=$(sed -n '12p' server.config);
 if [ $configData = 'Y' ]
 then
     sh dockerInstallation.sh;
