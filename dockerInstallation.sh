5eed71606c0df0953d3e052d923c92fe18ac34e7
kameshX007 <kameshbook@gmail.com>
2022-09-25 14:49:26 +0530

    #

--- dockerInstallation.sh
+++ dockerInstallation.sh
@@ -3,7 +3,7 @@
 echo "Installig Docker..."
 git clone https://github.com/kameshX007/installDocker.git;
 cd installDocker;
-sh installDocker.sh;
+bash installDocker.sh;
 cd ..
 echo "Docker installed successfully..."
 exit 0
